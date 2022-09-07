5b9ee546f0a0302d72418a62ca5d45c7a897c358
Raymond <ruiwenhw@gmail.com>
2022-09-07 15:48:16 -0700

    added #606

---
+++ python/0606_Construct_String_from_Binary_Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        if root is None:
+            return ""
+        res = str(root.val)
+        if root.left:
+            res += "(" + self.tree2str(root.left) + ")"
+        if root.right:
+            if not root.left:
+                res += "()"
+            res += "(" + self.tree2str(root.right) + ")"
+        return res
