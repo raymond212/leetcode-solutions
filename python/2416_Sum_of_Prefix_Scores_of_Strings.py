344a40d1aea559b9c9eab2524b068a35f65d98b4
Raymond <ruiwenhw@gmail.com>
2022-09-17 21:41:12 -0700

    updated #2416

--- python/2416_Sum_of_Prefix_Scores_of_Strings.py
+++ python/2416_Sum_of_Prefix_Scores_of_Strings.py
@@ -1,4 +1,31 @@
-class Solution:
+# Using Trie
+class Solution1:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        trie = {}
+        for word in words:
+            cur = trie
+            for c in word:
+                if c not in cur:
+                    cur[c] = [{}, 0]
+                cur[c][1] += 1
+                cur = cur[c][0]
+        
+        res = []
+        for word in words:
+            score_sum = 0
+            cur = trie
+            for c in word:
+                if c not in cur:
+                    break
+                nxt, score = cur[c]
+                score_sum += score
+                cur = nxt
+            res.append(score_sum)
+        return res
+    
+
+# Using prefix frequency dict
+class Solution2:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
         freq = {}
         for word in words:
