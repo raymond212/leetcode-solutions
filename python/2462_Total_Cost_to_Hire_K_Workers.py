63bd1feaf573efdfb95d5f51dd70c0360b0a2348
Raymond <ruiwenhw@gmail.com>
2022-11-06 10:06:37 -0800

    Updated #2462

--- python/2462_Total_Cost_to_Hire_K_Workers.py
+++ python/2462_Total_Cost_to_Hire_K_Workers.py
@@ -1,33 +1,28 @@
-import bisect
+import heapq
 
 class Solution:
     def totalCost(self, costs, k: int, candidates: int) -> int:
         n = len(costs)
-        
-        if 2 * candidates >= n: 
-            costs.sort()
-            return sum(costs[:k])
-        
         first = sorted(costs[:candidates])                     
-        last = sorted(costs[n - candidates:])
+        last = sorted(costs[max(candidates, n - candidates):])
+        heapq.heapify(first)
+        heapq.heapify(last)
         
         firstAdd = candidates # next element that gets merged into first
         lastAdd = n - candidates - 1 # next element that gets merged into last
 
         res = 0
 
-        for i in range(k):
-            if len(last) == 0 or (len(first) > 0 and first[0] <= last[0]):  # if best candidate is in first part
-                res += first[0]
-                first.pop(0)
+        for _ in range(k):
+            if not last or first and first[0] <= last[0]:  # if best candidate is in first part
+                res += heapq.heappop(first)
                 if firstAdd <= lastAdd:
-                    bisect.insort(first, costs[firstAdd])
+                    heapq.heappush(first, costs[firstAdd])
                     firstAdd += 1
             else:
-                res += last[0]
-                last.pop(0)
+                res += heapq.heappop(last)
                 if firstAdd <= lastAdd:
-                    bisect.insort(last, costs[lastAdd])
+                    heapq.heappush(last, costs[lastAdd])
                     lastAdd -= 1
 
         return res
