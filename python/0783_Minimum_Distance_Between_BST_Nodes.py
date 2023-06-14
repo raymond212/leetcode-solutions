dec4fdc3e5a4c97ea39db197431feb8b7e3107f9
Raymond <ruiwenhw@gmail.com>
2023-06-13 19:43:11 -0700

    Add #530, #783

---
+++ python/0783_Minimum_Distance_Between_BST_Nodes.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        nums = []
+        
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            nums.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+        ans = float('inf')
+        for i in range(len(nums) - 1):
+            ans = min(ans, nums[i + 1] - nums[i])
+        
+        return ans
