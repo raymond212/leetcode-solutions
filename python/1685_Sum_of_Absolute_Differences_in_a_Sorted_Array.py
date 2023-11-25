dba4e46c6a22252a4b6dc435695d07a9aa1623f4
Raymond <ruiwenhw@gmail.com>
2023-11-25 10:40:17 -0800

    Rename variable

--- python/1685_Sum_of_Absolute_Differences_in_a_Sorted_Array.py
+++ python/1685_Sum_of_Absolute_Differences_in_a_Sorted_Array.py
@@ -1,17 +1,17 @@
 class Solution:
     def getSumAbsoluteDifferences(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        pref = [0] * (n + 1)
+        pre = [0] * (n + 1)
 
         for i in range(1, n + 1):
-            pref[i] = pref[i - 1] + nums[i - 1]
+            pre[i] = pre[i - 1] + nums[i - 1]
 
         res = []
         for i, val in enumerate(nums):
             small = i
             large = n - small - 1
-            small_res = small * val - pref[small]
-            large_res = pref[n] - pref[n - large] - large * val
+            small_res = small * val - pre[small]
+            large_res = pre[n] - pre[n - large] - large * val
             res.append(small_res + large_res)
         
         return res
