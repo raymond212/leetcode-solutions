7909b62593952b528176542a30e5fbbbe79edefe
Raymond <ruiwenhw@gmail.com>
2023-12-05 20:33:28 -0800

    Edit #1716

--- python/1716_Calculate_Money_in_Leetcode_Bank.py
+++ python/1716_Calculate_Money_in_Leetcode_Bank.py
@@ -1,4 +1,8 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
+        # sum of integers from 1 to x, inclusive
+        def calc(x):
+            return x * (x + 1) // 2
+
         a, b = n // 7, n % 7
-        return a * 28 + 7 * a * (a - 1) // 2 + b * (b + 1) // 2 + b * a
+        return a * 28 + 7 * calc(a - 1) + calc(b) + b * a
