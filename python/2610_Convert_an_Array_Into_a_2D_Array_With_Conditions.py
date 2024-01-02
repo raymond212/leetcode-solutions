3aee95966db08e9513dae3641a40c59fb47d5c74
Raymond <ruiwenhw@gmail.com>
2024-01-01 17:15:21 -0800

    Add #2610

---
+++ python/2610_Convert_an_Array_Into_a_2D_Array_With_Conditions.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        count = Counter(nums)
+        k = max(count.values())
+        A = list(count.elements())
+        return [A[i::k] for i in range(k)]
